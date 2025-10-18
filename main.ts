6068da03c002d3ec781560faf4ead8e425f73ad9
Tarosa <tarosa.y@gmail.com>
2025-10-19 07:07:53 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,21 +5,21 @@ radio.onReceivedNumber(function (receivedNumber) {
 // +が前に進む、-が後に進む
 function 動く (右タイヤ: number, 左タイヤ: number) {
     if (右タイヤ >= 0) {
-        pins.digitalWritePin(DigitalPin.P14, 0)
-        pins.digitalWritePin(DigitalPin.P15, 1)
-        動く右 = 右タイヤ
-    } else {
-        pins.digitalWritePin(DigitalPin.P14, 1)
-        pins.digitalWritePin(DigitalPin.P15, 0)
-        動く右 = 0 - 右タイヤ
-    }
-    if (左タイヤ >= 0) {
         pins.digitalWritePin(DigitalPin.P8, 0)
         pins.digitalWritePin(DigitalPin.P9, 1)
-        動く左 = 左タイヤ
+        動く右 = 右タイヤ
     } else {
         pins.digitalWritePin(DigitalPin.P8, 1)
         pins.digitalWritePin(DigitalPin.P9, 0)
+        動く右 = 0 - 右タイヤ
+    }
+    if (左タイヤ >= 0) {
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        動く左 = 左タイヤ
+    } else {
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        pins.digitalWritePin(DigitalPin.P15, 0)
         動く左 = 0 - 左タイヤ
     }
     P13_PWM = 1023 * (動く左 / 100)
