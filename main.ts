63dfc024636e8c5df2ed42e3d015ba725480f134
Tarosa <tarosa.y@gmail.com>
2025-10-19 06:59:33 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 radio.onReceivedNumber(function (receivedNumber) {
     動く(receivedNumber, receivedNumber)
 })
+// タイヤの回転は、0～100% の範囲で設定します
+// +が前に進む、-が後に進む
 function 動く (右タイヤ: number, 左タイヤ: number) {
     if (右タイヤ >= 0) {
         pins.digitalWritePin(DigitalPin.P14, 0)
@@ -35,7 +37,11 @@ let P16_PWM = 0
 let P13_PWM = 0
 let 動く左 = 0
 let 動く右 = 0
-if (input.buttonIsPressed(Button.A)) {
+if (input.buttonIsPressed(Button.B)) {
+    while (true) {
+        動く(90, 90)
+    }
+} else if (input.buttonIsPressed(Button.A)) {
     radio.setGroup(3)
     basic.showNumber(3)
 } else {
